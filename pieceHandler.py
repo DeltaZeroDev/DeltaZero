8dfaf46ea261e54f96bd942d3a3bc8069484417e
Max <106037550+ZeroSkillCoder@users.noreply.github.com>
2023-05-31 14:54:58 +0100

    Added material values to all pieces

--- pieceHandler.py
+++ pieceHandler.py
@@ -7,6 +7,56 @@ class pawn():
         self.material = 1
         self.color = color
         self.moved = False
+        
+class knight():
+    def __init__(self, x, y, color) -> None:
+        self.x = x
+        self.y = y
+        self.type = "knight"
+        self.typeShort = "h"
+        self.material = 3
+        self.color = color
+        self.moved = False
+        
+class bishop():
+    def __init__(self, x, y, color) -> None:
+        self.x = x
+        self.y = y
+        self.type = "bishop"
+        self.typeShort = "b"
+        self.material = 3
+        self.color = color
+        self.moved = False
+        
+class rook():
+    def __init__(self, x, y, color) -> None:
+        self.x = x
+        self.y = y
+        self.type = "rook"
+        self.typeShort = "r"
+        self.material = 5
+        self.color = color
+        self.moved = False
+        
+ class queen():
+    def __init__(self, x, y, color) -> None:
+        self.x = x
+        self.y = y
+        self.type = "queen"
+        self.typeShort = "q"
+        self.material = 9
+        self.color = color
+        self.moved = False
+ 
+class king():
+    def __init__(self, x, y, color) -> None:
+        self.x = x
+        self.y = y
+        self.type = "pawn"
+        self.typeShort = "k"
+        self.material = 999
+        self.color = color
+        self.moved = False
 
     def __dict__(self) -> dict:
         return {"x": self.x, "y": self.y, "type": self.type, "color": self.color, "moved": self.moved, "typeShort": self.typeShort, "material": self.material}
