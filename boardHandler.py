40e5cfe9914fb577f5d8bd2cd5f8c32c2889ad1b
Max <106037550+ZeroSkillCoder@users.noreply.github.com>
2023-05-31 15:40:05 +0100

    Added other pieces and made comments insulting ace :)

--- boardHandler.py
+++ boardHandler.py
@@ -11,7 +11,7 @@ class main():
         else:
             return self.board[x][y]
     
-    def setPos(self, x, y, value):
+    def setPos(self, y, x, value):
         if x < 0 or x > 7 or y < 0 or y > 7:
             return None
         else:
@@ -30,7 +30,30 @@ class main():
         return board
     
     def placePieces(self):
-        # place pawns
+        # THIS IDIOT DOESN'T KNOW HOW COORDINATES WORK HELP ME PLEASE - Zero
+        # (He flipped the x and y corrdinates around and reversed them even after I fixed it) - Zero
         for i in range(8):
-            self.setPos(1, i, ph.pawn(1, i, "white"))
-            self.setPos(6, i, ph.pawn(6, i, "black"))
+            # place pawns
+            self.setPos(i, 1, ph.pawn(i, 1, "white"))
+            self.setPos(i, 6, ph.pawn(i, 6, "black"))
+        # knights
+        self.setPos(6, 0, ph.knight(7, 0, "white")) 
+        self.setPos(1, 0, ph.knight(2, 0, "white"))
+        self.setPos(1, 7, ph.knight(2, 7, "black"))
+        self.setPos(6, 7, ph.knight(7, 7, "black"))
+        # bishops
+        self.setPos(5, 0, ph.bishop(6, 0, "white"))
+        self.setPos(2, 0, ph.bishop(6, 0, "white"))
+        self.setPos(2, 7, ph.bishop(6, 0, "black"))
+        self.setPos(5, 7, ph.bishop(6, 7, "black"))
+        # queens
+        self.setPos(3, 0, ph.queen(4, 0, "white"))
+        self.setPos(3, 7, ph.queen(4, 7, "black"))
+        # kings
+        self.setPos(4, 0, ph.king(5, 0, "white"))
+        self.setPos(4, 7, ph.king(5, 7, "black"))
+        # rooks
+        self.setPos(0, 0, ph.rook(1, 0, "white"))
+        self.setPos(7, 0, ph.rook(7, 0, "white"))
+        self.setPos(0, 7, ph.rook(0, 7, "black"))
+        self.setPos(7, 7, ph.rook(7, 7, "black"))
